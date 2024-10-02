8c491560e1f8907824f1d200b95390f4907ec6a7
smusp <smuspratt54@gmail.com>
2024-10-02 15:49:18 +1000

    edit README

---
+++ logo/hex.r
@@ -0,0 +1,10 @@
+
+library(hexSticker)
+
+sticker("Mediation.svg", package = "SEMs with lavaan",
+             filename = "hex.svg",
+			 s_x = 1, s_y = 1.35, s_width = 0.7, s_height = 0.7,
+			 p_x = 1, p_y = 0.6,
+             p_color = "white", p_size = 4.5,
+			 h_color = "grey", h_fill = "#CB6015")
+			 
